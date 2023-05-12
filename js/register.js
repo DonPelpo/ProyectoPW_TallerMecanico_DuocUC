0d558192da3bbca013a9d5c26f21bdc9893d5a45
Pablo Cabello <pablo.cabello.garcia14@gmail.com>
2023-05-12 16:49:10 -0600

    Validacion Administrador, navegar entre paginas, pagina de contacto

--- js/register.js
+++ js/register.js
@@ -1,31 +1,14 @@
 
 
-function verificar() {
-    if ( $("#exampleInputEmail1").val().trim().length = 0 ) {
-      alert("El campo contiene un string válido que no son espacios");
-    }
-    else {
-      alert("El campo contiene espacios y está vacío");
-    }
-
+function verificar(rut, password) {
+  if(rut == '12345' && password == '54321'){
+      window.location.replace("./administradorPage.html")
   }
-
-
-  const users = [
-    { user: 'ss', password: '22' },
-    { user: 'aa', password: '11' },
-];
-
-const validation = (user, pass) => users.find( ( person ) => (person.user === user) && (person.password === pass));
-
-const login = () => {
-    const user = document.getElementById( 'txt' ).value;
-    const pass = document.getElementById( 'password' ).value;
-    if ( validation(user, pass) !== undefined ) {
-        href="./index.html"
-        alert( `El usuario ${user} es correcto |nais|` )
-    }
-    else {
-        alert( 'Usuario o contraseña incorrectos...' );
-    }
+  else{
+    alert("Información invalida");
+    return;
+  }
 }
+
+
+  
