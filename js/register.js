c74ccb726e7fb6b62b105d9d6c9ba460fa697761
Pablo Cabello <pablo.cabello.garcia14@gmail.com>
2023-05-12 01:59:14 -0600

    ta maomeno pero mejorando falta validar rut y contraseña y que al validarlo se pueda navegar entre pantallas

--- js/register.js
+++ js/register.js
@@ -1,13 +1,31 @@
-const nombre= document.getElementById("name")
-const email= document.getElementById("email")
-const pass= document.getElementById("password")
-const form = document.getElementById ("form")
-const parrafo = document.getElementById("warnings")
 
-form.addEventListener("enviar", e => 
-{
-    e.preventDefault()
-    if (nombre.Value.length < 6){
-        alert("nombtr muy cortos")
+
+function verificar() {
+    if ( $("#exampleInputEmail1").val().trim().length = 0 ) {
+      alert("El campo contiene un string válido que no son espacios");
     }
-} )
+    else {
+      alert("El campo contiene espacios y está vacío");
+    }
+
+  }
+
+
+  const users = [
+    { user: 'ss', password: '22' },
+    { user: 'aa', password: '11' },
+];
+
+const validation = (user, pass) => users.find( ( person ) => (person.user === user) && (person.password === pass));
+
+const login = () => {
+    const user = document.getElementById( 'txt' ).value;
+    const pass = document.getElementById( 'password' ).value;
+    if ( validation(user, pass) !== undefined ) {
+        href="./index.html"
+        alert( `El usuario ${user} es correcto |nais|` )
+    }
+    else {
+        alert( 'Usuario o contraseña incorrectos...' );
+    }
+}
