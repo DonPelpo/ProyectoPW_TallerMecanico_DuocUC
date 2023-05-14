1a74bfde70e23b1e9bb624021330c1c3f536a96b
Pablo Cabello <pablo.cabello.garcia14@gmail.com>
2023-05-14 10:02:12 -0600

    login validado, cuenta administrador mecanico, ficha de mantencion

--- js/register.js
+++ js/register.js
@@ -1,12 +1,17 @@
 
 
 function verificar(rut, password) {
-  if(rut == '12345' && password == '54321'){
-      window.location.replace("./administradorPage.html")
+  if(rut == '12345' && password == '54321' ){
+      window.location.replace("./administrador./administradorPage.html")
   }
   else{
-    alert("Información invalida");
-    return;
+    if(rut == '123456' && password == '654321'){
+      window.location.replace("./mecanico/mecanicoPage.html")
+    }
+    else{
+      alert("Información invalida");
+      return;
+    }
   }
 }
 
